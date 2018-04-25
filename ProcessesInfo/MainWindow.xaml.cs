03a1e8e42d024cc293fdd41634be54842b3c4acc
Niyaz <33822162+serenity8@users.noreply.github.com>
2018-04-25 10:34:23 +0600

    Initial commit

---
+++ ProcessesInfo/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace ProcessesInfo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+
+
+        }
+
+        private void ShowProcesses_Click(object sender, RoutedEventArgs e)
+        {
+            List<InfoProcessModel> infoModels = new List<InfoProcessModel>();
+            var processes = Process.GetProcesses();
+
+            infoModels = processes.Select(x => new InfoProcessModel()
+            {
+                Id = x.Id,
+                Name = x.ProcessName,
+                NumberOfThreads = x.Threads.Count
+            }).ToList();
+
+            InfoProcesses.ItemsSource = infoModels;
+            //Select(x => new UserQueryView()
+            //{
+            //    Id = x.Id,
+            //    QueryText = x.QueryText,
+            //    DateWritten = x.DateWritten,
+            //    QueryStatus = x.QueryStatus
+            //})
+
+        }
+    }
+
+    public class InfoProcessModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NumberOfThreads { get; set; }
+    }
+}
